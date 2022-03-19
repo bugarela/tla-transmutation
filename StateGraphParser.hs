1068a95d2f4038f34cd882ae4f8e49c1beb221e6
Gabriela Mafra <gabrielamoreiramafra@gmail.com>
2022-03-19 19:39:29 -0300

    Use either to propagate errors

--- StateGraphParser.hs
+++ StateGraphParser.hs
@@ -1,17 +1,10 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-import qualified Head as H
-import qualified Math as H
 import Data.Aeson
 import Data.List
 import GHC.Generics
 import qualified Data.ByteString.Lazy as B
-import Control.Monad
-import Control.Applicative
-import Control.Monad.Instances
-
-import Parser
 
 jsonFile :: FilePath
 jsonFile = "tla_specifications/states.json"
@@ -19,9 +12,9 @@ jsonFile = "tla_specifications/states.json"
 getJSON :: IO B.ByteString
 getJSON = B.readFile jsonFile
 
-data Graph = Graph { nodes :: [Node], edges :: [Edge] } deriving (Show,Generic)
-data Node = Node { nodeId :: Integer, label :: String } deriving (Show,Generic)
-data Edge = Edge { nodeFrom :: Integer, nodeTo:: Integer } deriving (Show,Generic)
+data Graph = Graph { nodes :: [Node], edges :: [Edge] } deriving (Show, Generic)
+data Node = Node { nodeId :: Integer, label :: String } deriving (Show, Generic)
+data Edge = Edge { nodeFrom :: Integer, nodeTo :: Integer } deriving (Show, Generic)
 
 instance FromJSON Graph where
    parseJSON (Object v) =
@@ -39,21 +32,31 @@ instance FromJSON Edge where
          <*> v .: "head"
 
 
-genTests :: Graph -> String
-genTests (Graph{nodes=ns, edges=es}) = let ts = map (testForNode es ns) ns in intercalate "\n" ts
+genTests :: Graph -> Either String String
+genTests Graph{nodes=ns, edges=es} = case traverse (testForNode (Graph ns es)) ns of
+                                         Right ts -> Right (intercalate "\n" ts)
+                                         Left e -> Left e
 
-testForNode es ns (Node{nodeId=id, label=l}) = let ss = statesFromId es ns id
-  in "variables = " ++ l ++ "\n expectedStates = [" ++ intercalate "\n" ss ++ "]"
+testForNode :: Graph -> Node -> Either String String
+testForNode g Node{nodeId=i, label=l} = case statesFromId g i of
+                                            Right ss -> Right ("variables = " ++ l ++ "\n expectedStates = [" ++ intercalate "\n" (map show ss) ++ "]")
+                                            Left e -> Left e
 
-statesFromId :: [Edge] -> [Node] -> Integer -> [String]
-statesFromId es ns i = map (\Edge{nodeFrom=_, nodeTo=t} -> show (findNode ns t)) (filter (\Edge{nodeFrom=f, nodeTo=t} -> f == i) es)
+statesFromId :: Graph -> Integer -> Either String [Node]
+statesFromId Graph{nodes=ns, edges=es} i = let edgesFromId = filter (\Edge{nodeFrom=f, nodeTo=_} -> f == i) es
+                                               nodesIdsFromId = map (\Edge{nodeFrom=_, nodeTo=t} -> t) edgesFromId
+                                             in traverse (findNode ns) nodesIdsFromId
 
-findNode :: [Node] -> Integer -> Maybe Node
-findNode ns n = find (\Node{nodeId=id, label=_} -> n == id) ns
+findNode :: [Node] -> Integer -> Either String Node
+findNode ns n = case find (\Node{nodeId=i, label=_} -> n == i) ns of
+                  Just node -> Right node
+                  Nothing -> Left ("Node with id " ++ show n ++ " could not be found")
 
 main :: IO ()
 main = do
  d <- (eitherDecode <$> getJSON) :: IO (Either String Graph)
  case d of
   Left err -> putStrLn err
-  Right ps -> putStrLn (genTests ps)
+  Right ps -> case genTests ps of
+    Left err -> putStrLn err
+    Right s -> putStrLn s
